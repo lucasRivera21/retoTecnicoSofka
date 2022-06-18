ca6cf928d6997c092c17f8c96e244bf49871c713
lucasRivera21 <lucasriverar2001@gmail.com>
2022-06-18 12:32:13 -0500

    Actualizacion js

--- js/index.js
+++ js/index.js
@@ -1,4 +1,82 @@
-const formulario = document.getElementById('formulario');
+function borrar(){                              //limpia los inputs
+    document.getElementById("name").value = "";
+    document.getElementById("pais").value = "";
+    document.getElementById("peso").value = "";
+    document.getElementById("combustible").value = "";
+}
+
+
+function captura(){
+    var tipo;
+    var nombre = document.getElementById("name").value;             //nombre de la nave
+
+    var elementos = document.getElementsByName("tipo");             //Tipo de Naves
+    for(let i=0; i<elementos.length; i++){
+        if(elementos[i].checked){
+            tipo = elementos[i].value;
+        }    
+    }
+    console.log(document.getElementsByName("tipo").value);
+
+    var pais = document.getElementById("pais").value;               //pais de origen de la nave
+    var peso = document.getElementById("peso").value;               //peso de la nave
+
+    var combustible = document.getElementById("combustible").value; //combustible que usa la nave
+    
+    
+    let nave1 = new Nave(nombre, tipo, pais, peso, combustible);    //nave1 instanciada
+
+    const infoNave = document.getElementById("navesContainer");
+    const div = document.createElement("div");
+    const nombreDiv = document.createElement("h3");
+    const parrafoDiv = document.createElement("p");
+    const salto = document.createElement("br");
+    
+    infoNave.style.backgroundColor = "#182693";
+    infoNave.style.border = "#141527 10px solid";
+
+    div.style.padding = "10px";
+    div.style.borderBottom = "3px solid #141527"
+
+    nombreDiv.textContent = nave1.nombre;
+
+    if(nave1.tipo == "lanzadera"){
+        nave1.tipo = "Vehículo lanzadera";
+    }else if(nave1.tipo == "no-tripulado"){
+        nave1.tipo = "Vehículo no tripulado";
+    }else{
+        nave1.tipo = "Vehículo tripulado";
+    }
+
+    parrafoDiv.textContent = "TIPO: " + nave1.tipo + ",  PAIS: " + nave1.pais + ",  PESO: " + nave1.peso + "Kg ,  COMBUSTIBLE: " + nave1.combustible;
+    
+    div.appendChild(nombreDiv);
+    div.appendChild(parrafoDiv);
+    infoNave.appendChild(div);
+    
+    borrar();
+    
+}
+
+formulario.addEventListener('submit', (e) => {
+    e.preventDefault();                         //Evita recargar la página
+});
+
+class Nave{                             //plantilla nave
+    constructor(nombre, tipo, pais, peso, combustible){
+        this.nombre = nombre;
+        this.tipo = tipo;
+        this.pais = pais;
+        this.peso = peso;
+        this.combustible = combustible;
+    }
+}
+
+
+
+
+
+/*const formulario = document.getElementById('formulario');
 const inputs = document.querySelectorAll('#formulario input');
 
 console.log(inputs);
@@ -24,15 +102,7 @@ formulario.addEventListener('submit', (e) => {
 
 let bool = false;
 
-class Nave{
-    constructor(nombre, tipo, pais, peso, combustible){
-        this.nombre = 'nombre';
-        this.tipo = tipo;
-        this.pais = 'pais';
-        this.peso = peso;
-        this.combustible = 'combustible';
-    }
-}
+
 
 /*boton.addEventListener("click", () => {
     console.log("Hay click");
